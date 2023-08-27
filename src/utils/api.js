98a63d369660e54adea7d46ad7f0c6e8fb43b7c2
Hardik Choudhary <hardik109bkp@gmail.com>
2023-08-27 23:42:48 +0530

    important update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-// const TMDB_TOKEN = import.meta.env.VEET_APP_TMDB_TOKEN;
-const TMDB_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYzFmODA0MDY2ODQ2NTZhMTEwNzQ5NGFiOTYzYjQ1MyIsInN1YiI6IjY0ZTFkYTY0ZTE5ZGU5MDEzYTI4ZGIwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RgsyYcOEXgaQyoAFR9LPyU-OXCWaqEhGNORU28BxI1A";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
 
 const headers = {
   Authorization: `bearer ${TMDB_TOKEN}`,
--- src/utils/streamapi.js
+++ src/utils/streamapi.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "https://streaming-availability.p.rapidapi.com";
 
+const RapidAPI_Key = import.meta.env.VITE_APP_X_RAPIDAPI_KEY;
 const headers = {
-  "X-RapidAPI-Key": "8f00895bc1msh740bc46a053e615p1e3ef5jsne002edc75198",
+  "X-RapidAPI-Key": `${RapidAPI_Key}`,
 };
 
 
